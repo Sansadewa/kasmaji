b3901fa781b4606956f543c73a3224675008e539
Gibran Sansadewa Asshadiqi <15.8636@stis.ac.id>
2020-05-18 08:50:21 +0800

    nambah input post

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -27,51 +27,6 @@ class Register extends CI_Controller {
 		}
 	}
 
-	//Ini manggil halaman buat input email untuk dikirimkan kode aktivasi
-	// public function request(){
-	// 	$this->load->view('request');
-	// }
-
-
-	//Ini disimpen, siapatau maunya aktivasi via email.
-	//Ini mengirimkan kode aktivasi via email.
-	// public function process(){
-	// 	$nim=$this->input->post('nim');
-
-	// 	$this->load->library('email');
-	//     $surat['protocol']    = 'smtp';
-	//     $surat['smtp_host']    = 'ssl://mail.stislimatuju.com';
-	//     $surat['smtp_port']    = '465';
-	//     $surat['smtp_user']    = 'no-reply@stislimatuju.com';
-	//     $surat['smtp_pass']    = 'tgUBET9qAbCP5Lh';
-	//     $surat['charset']    = 'utf-8';
-	//     $surat['newline']  = "\r\n";
-	//     $surat['validate'] = TRUE;
-    // 	$this->email->initialize($surat);
-
-    // 	$q=$this->register_model->get_profile($nim);
-
-    // 	$data['code']=md5($nim.$q->ayah);
-    // 	$data['nim']=$nim;
-    // 	$this->email->from('no-reply@stislimatuju.com','STISLIMATUJU');
-	//     $this->email->subject('Kode Unik SIPAJU');
-	//     $this->email->message($this->load->view('email_daftar',$data,true));
-	//     $this->email->set_mailtype("html");
-	//     $this->email->to($nim.'@stis.ac.id');
-
-	//     if(!$this->email->send()){
-	//       echo "Mailer Error: " . $this->email->print_debugger();
-	//       echo "Gagal";
-	//       redirect('Login');
-	//     }
-	//     else{
-	//     	$this->register_model->update_unik($nim,$data['code']);
-	//       $this->session->set_flashdata('report','Kode Unik sedang dikirimkan ke email STIS anda. Mohon tunggu beberapa menit.');
-	//       redirect('register');
-	//     }
-
-	// }
-
 	public function first() {
 		$aku=$this->register_model->get_info($this->session->userdata('username'));
 		//yang boleh kesini cuma yang step=0, yg belom pernah masuk samsasekali.
@@ -82,6 +37,9 @@ class Register extends CI_Controller {
 	public function procpass() {
 		$profil=$this->register_model->get_info($this->session->userdata('username'));
 		if($profil->step==0){
+
+			//BELOM DIAMANKAN ISINYA.
+
 			$pa=$this->input->post('pa');
 			$pb=$this->input->post('pb');
 			$email=$this->input->post('email');
@@ -103,7 +61,29 @@ class Register extends CI_Controller {
 		//yang boleh kesini cuma yang step=1, yg belom pernah isi biodata dasar
 		if ($aku->step!=1) redirect('register');
  		$this->load->view('secondblank');	
-	}	
+	}
+	
+	public function procbase(){
+		$profil=$this->register_model->get_info($this->session->userdata('username'));
+		if ($profil->step!=1) redirect('register');
+
+		//BELOM DIAMANKAN ISINYA
+
+		$nomorhp=$this->input->post('nomorhp');
+		$nomorwa=$this->input->post('nomorwa');
+		$linkedin=$this->input->post('linkedin');
+		$facebook=$this->input->post('facebook');
+		$ig=$this->input->post('ig');
+		$twitter=$this->input->post('twitter');
+		$prov=$this->input->post('prov');
+		$kabkot=$this->input->post('kabkot');
+		$alamat_lengkap=$this->input->post('alamat_lengkap');
+		$lanjut_belajar=$this->input->post('lanjut_belajar');
+		$kegiatan=$this->input->post('kegiatan');
+
+		$this->register_model->putprofil($nomorhp,$nomorwa,$linkedin,$facebook,$ig,$twitter,$prov,$kabkot,$alamat_lengkap,$lanjut_belajar,$kegiatan);
+
+	}
 
 	public function ah(){
 		$this->load->view('first');
